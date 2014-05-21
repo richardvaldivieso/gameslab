636ebd68023902b52cefe2214ccf20e65c6a8d7b
Richard Valdivieso <richardvaldiviesom@gmail.com>
2014-05-21 09:56:37 -0500

    First commit

---
+++ AngularDemo/AngularDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AngularDemo.Startup))]
+namespace AngularDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
