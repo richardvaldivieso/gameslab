b115288b2bf8d74d4054aa0b4939c5791b9573d9
Richard Valdivieso <richardvaldiviesom@gmail.com>
2014-09-15 20:09:39 -0400

    Issue 11: Application structure
    Creating a file structure. Nothing related to programming. I think when
    we start developing we need to do the pull request.

--- towerdefense/towerdefense/client/client.js
+++ towerdefense/towerdefense/client/client.js
@@ -3,17 +3,12 @@ if (Meteor.isClient) {
     return "Welcome to towerdefense.";
   };
 
+ 
   Template.hello.events({
     'click input': function () {
       // template data, if any, is available in 'this'
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
     }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
   });
 }
